fca20a385fea7ea86103de86a63b3f8f55b9f98d
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2016-05-25 17:36:09 -0400

    Adds ticket editing feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -25,7 +25,15 @@ class TicketsController < ApplicationController
   def edit
   end
 
-
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been updated."
+      render "edit"
+    end
+  end
 
   private
 
