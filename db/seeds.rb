57c785cc865b88d9b73cc316e6e04c3a72dab231
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2016-05-31 17:41:18 -0400

    Admins can edit project roles of existing users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@ unless User.exists?(email: "viewer@ticketee.com")
   User.create!(email: "viewer@ticketee.com", password: "password")
 end
 
+unless User.exists?(email: "admin@ticketee.com")
+  User.create!(email: "admin@ticketee.com", password: "admin123", admin: true)
+end
+
 ["Sublime Text 3", "Internet Explorer"].each do |name|
   unless Project.exists?(name: name)
     Project.create!(name: name, description: "A sample project about #{name}")
