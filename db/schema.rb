7646e27d611515674fe3a2b3636dfaf1e61772b3
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2016-06-02 09:47:56 -0400

    Add support for admins to set default state

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601212033) do
+ActiveRecord::Schema.define(version: 20160602132357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,9 @@ ActiveRecord::Schema.define(version: 20160601212033) do
   end
 
   create_table "states", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
+    t.string  "name"
+    t.string  "color"
+    t.boolean "default", default: false
   end
 
   create_table "tickets", force: :cascade do |t|
