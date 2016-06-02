072a74113d69d2131868cb055d36608600c363b5
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2016-06-02 10:24:19 -0400

    Support tagging tickets on creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602132357) do
+ActiveRecord::Schema.define(version: 20160602141905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,17 @@ ActiveRecord::Schema.define(version: 20160602132357) do
     t.string  "name"
     t.string  "color"
     t.boolean "default", default: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "tags_tickets", id: false, force: :cascade do |t|
+    t.integer "tag_id",    null: false
+    t.integer "ticket_id", null: false
+    t.index ["tag_id", "ticket_id"], name: "index_tags_tickets_on_tag_id_and_ticket_id", using: :btree
+    t.index ["ticket_id", "tag_id"], name: "index_tags_tickets_on_ticket_id_and_tag_id", using: :btree
   end
 
   create_table "tickets", force: :cascade do |t|
